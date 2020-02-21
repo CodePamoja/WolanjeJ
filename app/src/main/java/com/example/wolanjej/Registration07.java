b0d537041d15ad8a02d492b3c60402b44a8121a6
GloriaMuthumbi <gloria.muthumbi@strathmore.edu>
2020-02-21 14:20:53 +0300

    Reg07 commits

---
+++ app/src/main/java/com/example/wolanjej/Registration07.java
@@ -0,0 +1,14 @@
+package com.example.wolanjej;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration07 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration2);
+    }
+}
