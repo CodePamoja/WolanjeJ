e5da3f8612181037ce60c016da69d777490f005b
GlennOnyango <glennonyango@yahoo.com>
2020-02-21 15:38:43 +0300

    This has updated image conf and splash done

--- app/src/main/java/com/example/wolanjej/MainActivity.java
+++ app/src/main/java/com/example/wolanjej/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.wolanjej;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //setToolBar();
+        button = (Button)findViewById(R.id.btn_getstarted);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                movetoRegistration();
+            }
+        });
+    }
+
+
+    public void movetoRegistration() {
+        System.out.println("Button pressed");
+        Intent move = new Intent(this,Registartion06.class);
+        startActivity(move);
     }
 
     /*private void setToolBar() {
--- app/src/main/java/com/example/wolanjej/Registartion06.java
+++ app/src/main/java/com/example/wolanjej/Registartion06.java
@@ -3,21 +3,21 @@ package com.example.wolanjej;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ImageView;
 
 public class Registartion06 extends AppCompatActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registartion06);
         setToolBar();
 
-        mImageView.setImageResource(R.mipmap.group_6);
+        imageView = (ImageView)findViewById(R.id.image_holder);
+        imageView.setImageResource(R.mipmap.group_6);
     }
 
     private void setToolBar() {
@@ -34,7 +34,7 @@ public class Registartion06 extends AppCompatActivity {
     }
 
     //code for image
-    ImageView mImageView = (ImageView)findViewById(R.id.image_holder);
+    //ImageView mImageView = (ImageView)findViewById(R.id.image_holder);
 
 
 
