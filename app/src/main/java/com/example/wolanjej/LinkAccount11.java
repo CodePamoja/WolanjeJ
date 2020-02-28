5b3b90f93cd583790ee9b5405dd67af919c95573
unknown <kpaul7747@gmail.com>
2020-02-28 15:41:47 +0300

    Link title added

--- app/src/main/java/com/example/wolanjej/LinkAccount11.java
+++ app/src/main/java/com/example/wolanjej/LinkAccount11.java
@@ -3,6 +3,7 @@ package com.example.wolanjej;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -16,13 +17,28 @@ public class LinkAccount11 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_account11);
 
+        setToolBar();
     }
 
 
+    private void setToolBar() {
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        getSupportActionBar().setTitle("Link Account");
+//        final Intent movetoLogo = new Intent(this,MainActivity.class);
+//        tb.setNavigationOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        startActivity(movetoLogo);
+//                    }
+//                }
+//        );
+
+    }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.skip, menu);
         return true;
     }
 
-    //code for image
 }
