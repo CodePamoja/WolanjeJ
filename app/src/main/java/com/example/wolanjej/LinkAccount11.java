126d9d43a28c65faf582a110c7d6dbe968292081
unknown <kpaul7747@gmail.com>
2020-02-28 16:56:00 +0300

    Link Account cards onclicklistener added

--- app/src/main/java/com/example/wolanjej/LinkAccount11.java
+++ app/src/main/java/com/example/wolanjej/LinkAccount11.java
@@ -2,6 +2,7 @@ package com.example.wolanjej;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,14 +11,31 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class LinkAccount11 extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
 
+public class LinkAccount11 extends AppCompatActivity implements View.OnClickListener {
+
+    private MaterialCardView card_westernunion, card_worldplay, card_unionpay, card_mastercard, card_visa, card_paypal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_account11);
-
         setToolBar();
+        //defining cards
+        card_westernunion = (MaterialCardView) findViewById(R.id.card_westernunion);
+        card_worldplay = (MaterialCardView) findViewById(R.id.card_worldplay);
+        card_unionpay = (MaterialCardView) findViewById(R.id.card_unionpay);
+        card_mastercard = (MaterialCardView) findViewById(R.id.card_mastercard);
+        card_visa = (MaterialCardView) findViewById(R.id.card_visa);
+        card_paypal = (MaterialCardView) findViewById(R.id.card_paypal);
+
+        //addclick listener
+        card_westernunion.setOnClickListener(this);
+        card_worldplay.setOnClickListener(this);
+        card_unionpay.setOnClickListener(this);
+        card_mastercard.setOnClickListener(this);
+        card_visa.setOnClickListener(this);
+        card_paypal.setOnClickListener(this);
     }
 
 
@@ -39,6 +57,17 @@ public class LinkAccount11 extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.skip, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.card_westernunion: i = new Intent(this,Registration06.class);startActivity(i); break;
+            case R.id.card_worldplay: i = new Intent(this,Registration05.class);startActivity(i); break;
+            default:break;
+        }
     }
 
 }
