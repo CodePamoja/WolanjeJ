800a9035864907d528ed7880e3c21c5aa9725476
GlennOnyango <glennonyango@yahoo.com>
2020-03-04 11:54:24 +0300

    Navbar

--- app/src/main/java/com/example/wolanjej/Home.java
+++ app/src/main/java/com/example/wolanjej/Home.java
@@ -1,8 +1,11 @@
 package com.example.wolanjej;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
