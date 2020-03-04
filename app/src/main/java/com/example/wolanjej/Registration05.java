ecee0b58c499113222980fe1eaa58f15659b126f
unknown <kpaul7747@gmail.com>
2020-03-04 15:35:43 +0300

    adding okhttp files

--- app/src/main/java/com/example/wolanjej/Registration05.java
+++ app/src/main/java/com/example/wolanjej/Registration05.java
@@ -5,16 +5,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Registration05 extends AppCompatActivity {
 
     private ImageView imageView;
+    private EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,14 @@ public class Registration05 extends AppCompatActivity {
         setToolBar();
         imageView = (ImageView)findViewById(R.id.image_holder);
         imageView.setImageResource(R.mipmap.group_5);
+
+        Button btn = findViewById(R.id.btn_continue);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                sendToVerification();
+            }
+        });
     }
 
     private void setToolBar() {
@@ -55,8 +70,24 @@ public class Registration05 extends AppCompatActivity {
         startActivity(movetoLogo);
     }
 
-    public void sendtoVerification(View view) {
-        Intent movetoLogo = new Intent(this, Registration06.class);
-        startActivity(movetoLogo);
+    public void sendToVerification() {
+        text = findViewById(R.id.phoneNumber);
+        String value = text.getText().toString();
+        System.out.println(value);
+
+        JSONObject jValue = new JSONObject();
+        try {
+            jValue.put("phone", value);
+            Log.e("JValues",jValue.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+       OkhttpConnection okConn = new OkhttpConnection();
+        String result = okConn.postRequest(jValue.toString());
+        System.out.println(result);
+
+        /*Intent move = new Intent(this, Registration06.class);
+        startActivity(move);*/
     }
 }
