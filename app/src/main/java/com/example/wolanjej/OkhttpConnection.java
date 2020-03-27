83924f37341c7aedf8cfb5ff3500bde5e160b979
Paul kalenda <kpaul7747@gmail.com>
2020-03-27 19:13:14 +0300

    update on server and client connection

--- app/src/main/java/com/example/wolanjej/OkhttpConnection.java
+++ app/src/main/java/com/example/wolanjej/OkhttpConnection.java
@@ -21,49 +21,7 @@ import okhttp3.Response;
 
 public class OkhttpConnection {
     String baseUrl= "https://backoffice.wolenjeafrica.com/wolenje";
-//    OkHttpClient client = new OkHttpClient();
-    OkHttpClient clientSSL = new OkHttpClient();
-//    private static OkHttpClient getUnsafeOkHttpClient() {
-//        try {
-//            // Create a trust manager that does not validate certificate chains
-//            final TrustManager[] trustAllCerts = new TrustManager[] {
-//                    new X509TrustManager() {
-//                        @Override
-//                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-//                        }
-//
-//                        @Override
-//                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-//                        }
-//
-//                        @Override
-//                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-//                            return new java.security.cert.X509Certificate[]{};
-//                        }
-//                    }
-//            };
-//
-//            // Install the all-trusting trust manager
-//            final SSLContext sslContext = SSLContext.getInstance("SSL");
-//            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-//            // Create an ssl socket factory with our all-trusting manager
-//            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-//
-//            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-//            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
-//            builder.hostnameVerifier(new HostnameVerifier() {
-//                @Override
-//                public boolean verify(String hostname, SSLSession session) {
-//                    return true;
-//                }
-//            });
-//
-//            OkHttpClient okHttpClient = builder.build();
-//            return okHttpClient;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    OkHttpClient client = new OkHttpClient();
 
     public  Response postRequest(String url, String jsonbody){
         Response result = null;
@@ -76,7 +34,7 @@ public class OkhttpConnection {
                     .post(body)
                     .build();
 
-            Call call = clientSSL.newCall(request);
+            Call call = client.newCall(request);
             Response response = call.execute();
             Log.e("TAG", String.valueOf(response.code()));
             result  = response;
@@ -100,7 +58,7 @@ public class OkhttpConnection {
                     .post(body)
                     .build();
 
-            Call call = clientSSL.newCall(request);
+            Call call = client.newCall(request);
             Response response = call.execute();
             String test = response.body().string();
             Log.d("TAG", test);
