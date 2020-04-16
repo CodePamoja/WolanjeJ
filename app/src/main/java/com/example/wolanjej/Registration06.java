35368ff3da6ff08e6131caef158b19320c18632d
GlennOnyango <glennonyango@yahoo.com>
2020-04-16 16:41:24 +0300

    Edit Text made and works

--- app/src/main/java/com/example/wolanjej/Registration06.java
+++ app/src/main/java/com/example/wolanjej/Registration06.java
@@ -5,15 +5,18 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Registration06 extends AppCompatActivity {
 
-    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +25,98 @@ public class Registration06 extends AppCompatActivity {
 
         setContentView(R.layout.activity_registration06);
         setToolBar();
-        imageView = (ImageView)findViewById(R.id.image_holder);
+        ImageView imageView = findViewById(R.id.image_holder);
         imageView.setImageResource(R.drawable.ic_group_7__1_);
+
+        final EditText text1 = findViewById(R.id.edittext);
+        final EditText text2 =findViewById(R.id.inp2);
+        final EditText text3 = findViewById(R.id.inp3);
+        final EditText text4 = findViewById(R.id.inp4);
+
+
+        text1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                text1.setFocusable(false);
+                text1.setClickable(false);
+                text2.requestFocus();
+
+            }
+        });
+        text2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                text2.setFocusable(false);
+                text2.setClickable(false);
+                text3.requestFocus();
+            }
+        });
+        text3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                text3.setFocusable(false);
+                text3.setClickable(false);
+                text4.requestFocus();
+            }
+        });
+        text4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                text4.setFocusable(false);
+                text4.setClickable(false);
+                            }
+        });
+
+
     }
 
     private void setToolBar() {
-        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
         getSupportActionBar().setTitle("");
         final Intent movetoLogo = new Intent(this,Registration05.class);
@@ -46,4 +135,5 @@ public class Registration06 extends AppCompatActivity {
         Intent movetoLogo = new Intent(this,Registration07.class);
         startActivity(movetoLogo);
     }
+
 }
