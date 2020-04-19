656d8bb4e364f731a57ed0e5455ba31158d9fbd7
unknown <thuggeanalyst@gmail.com>
2020-04-20 01:08:32 +0300

    15 Ewallet2, 15 Ewallet2_1, 15 Ewallet3_2

---
+++ app/src/main/java/com/example/wolanjej/activity_15_Ewallet2.java
@@ -0,0 +1,14 @@
+package com.example.wolanjej;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_15_Ewallet2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_15__ewallet2);
+    }
+}
