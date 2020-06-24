91dd3971adcfeafc44623126416791327dffd498
GlennOnyango <glennonyango@yahoo.com>
2020-06-24 18:32:03 +0300

    'this

--- app/src/main/java/com/example/wolanjej/Bookflight06.java
+++ app/src/main/java/com/example/wolanjej/Bookflight06.java
@@ -7,21 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Bookflight04 extends AppCompatActivity {
+public class Bookflight06 extends AppCompatActivity {
 
 
     private Toolbar tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookflight04);
-//        setToolBar(tb);
+        setContentView(R.layout.activity_bookflight06);
+        setToolBar(tb);
     }
     private void setToolBar(androidx.appcompat.widget.Toolbar tb) {
         tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
         getSupportActionBar().setTitle("");
-        final Intent moveToLogo = new Intent(this,Education_50.class);
+        final Intent moveToLogo = new Intent(this,Bookflight05.class);
         tb.setNavigationOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -31,9 +31,5 @@ public class Bookflight04 extends AppCompatActivity {
                 }
         );
 
-    }
-
-    public void movetobook5(View view) {
-        startActivity(new Intent(this,Bookflight05.class));
     }
 }
