92f7970d19905c06b3db1267fd473340284b3c65
wamae77 <kelvinwamae77@gmail.com>
2020-07-17 00:07:57 +0300

    this

--- app/src/main/java/com/example/wolanjej/Profile49.java
+++ app/src/main/java/com/example/wolanjej/Profile49.java
@@ -8,19 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 
 public class Profile49 extends AppCompatActivity {
-
     private Toolbar tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile49);
+
+        setToolBar(tb);
     }
     public void MoveToProfile(View view){
         Intent intent = new Intent(getApplicationContext(), profile.class);
         startActivity(intent);
-        setToolBar(tb);
     }
     private void setToolBar(Toolbar tb) {
+        tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
         getSupportActionBar().setTitle("");
         final Intent movetoLogo = new Intent(this,profile.class);
