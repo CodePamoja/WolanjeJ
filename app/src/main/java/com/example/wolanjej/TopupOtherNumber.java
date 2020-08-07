c88f7547d19903ac17791d4b577ada966c4a1134
wamae77 <kelvinwamae77@gmail.com>
2020-08-07 21:28:11 +0300

    this

--- app/src/main/java/com/example/wolanjej/TopupOtherNumber.java
+++ app/src/main/java/com/example/wolanjej/TopupOtherNumber.java
@@ -1,8 +1,5 @@
 package com.example.wolanjej;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,12 +11,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.example.wolanjej.recyclerAdapters.SelectUserAdapter;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.OnItemSelectedListener  {
+public class TopupOtherNumber extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String[] selectNumber = {"Other Number", "My Number"};
 
     private String phoneName;
@@ -44,22 +44,28 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
         Intent intentExtra = getIntent();
         String className = getIntent().getStringExtra("Class");
         Log.e("class Type className", className);
-        if(className.equals("Top_up")) {
-            // todo here if there is any
-        }else if (className.equals("SelectUserAdapter")){
-            this.phoneNumber = intentExtra.getStringExtra(SelectUserAdapter.EXTRA_PHONE);
-            String userNumber = intentExtra.getStringExtra(SelectUserAdapter.EXTRA_PHONE);
-            this.phoneName = intentExtra.getStringExtra(SelectUserAdapter.EXTRA_NAME);
+        switch (className) {
+            case "Qr_code22":
 
-            EditText tvtext =  findViewById(R.id.transContactAmount);
-            tvtext.setText(userNumber);
+                break;
+            case "Top_up":
+                // todo here if there is any
+                break;
+            case "SelectUserAdapter":
+                this.phoneNumber = intentExtra.getStringExtra(SelectUserAdapter.EXTRA_PHONE);
+                String userNumber = intentExtra.getStringExtra(SelectUserAdapter.EXTRA_PHONE);
+                this.phoneName = intentExtra.getStringExtra(SelectUserAdapter.EXTRA_NAME);
+
+
+                EditText tvtext = findViewById(R.id.transContactAmount);
+                tvtext.setText(userNumber);
         }
 
         spin = (Spinner) this.findViewById(R.id.select_other_number);
         spin.setOnItemSelectedListener(this);
 
         //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,selectNumber);
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, selectNumber);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         spin.setAdapter(aa);
@@ -68,10 +74,9 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
     }
 
 
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if ("My Number".equals(selectNumber[position])){
+        if ("My Number".equals(selectNumber[position])) {
             Intent move = new Intent(this, Top_up.class);
             startActivity(move);
         }
@@ -84,7 +89,7 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
 
     public void moveToContact(View view) {
         Intent move = new Intent(this, ContactsView.class);
-        move.putExtra("Class","TopUpOtherNumber");
+        move.putExtra("Class", "TopUpOtherNumber");
         move.putExtra(EXTRA_CLASSTYPE, "TopUpNumber");
         startActivity(move);
     }
@@ -99,15 +104,15 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
         Log.e("TAG phone number check", "button pressed to transfer money" + phone);
         String phonenumber = changePhoneNo(phone, view);
         Log.e("TAG phone number last", phonenumber);
-        int x =Integer.parseInt(amount);
-        if (amount!=null){
-            if (x>=10){
-                if (x<=70000){
-                    if(phonenumber!="Fasle"){
+        int x = Integer.parseInt(amount);
+        if (amount != null) {
+            if (x >= 10) {
+                if (x <= 70000) {
+                    if (phonenumber != "Fasle") {
                         movetoPin(phonenumber, amount);
                     }
-                }else{
-                    Toast.makeText(getApplicationContext(),"The Amount is above 70000" , Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "The Amount is above 70000", Toast.LENGTH_LONG).show();
                 }
             } else {
                 Toast.makeText(getApplicationContext(), "The Amount is below 10", Toast.LENGTH_LONG).show();
@@ -119,7 +124,7 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
 
     }
 
-    public String changePhoneNo(String inputPhone, View view){
+    public String changePhoneNo(String inputPhone, View view) {
         String validPhoneNo = "Fasle";
         String safaricom = "^(?:254|\\+254|0)?(7(?:(?:[129][0-9])|(?:0[0-9])|(?:6[8-9])|(?:5[7-9])|(?:4[5-6])|(?:4[8])|(4[0-3]))[0-9]{6})$";
         String telkom = "^(?:254|\\+254|0)?(7(?:(?:[7][0-9]))[0-9]{6})$";
@@ -135,16 +140,16 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
                 Toast.makeText(getApplicationContext(), "Safaricom Number", Toast.LENGTH_LONG).show();
                 String replPhone3 = "null";
                 phoneCompany = "safaricom";
-                if(replPhone2.startsWith("0")){
+                if (replPhone2.startsWith("0")) {
                     replPhone3 = replPhone2.replaceFirst("0", "\\254");
                     Log.e("TAG phone starts 0", replPhone3);
                     validPhoneNo = replPhone3;
-                }else if(replPhone2.startsWith("7")){
+                } else if (replPhone2.startsWith("7")) {
                     replPhone3 = replPhone2.replaceFirst("7", "\\254");
                     Log.e("TAG phone starts 7", replPhone3);
                     validPhoneNo = replPhone3;
-                }else if(replPhone2.startsWith("+")){
-                    validPhoneNo = replPhone2.replaceAll("[\\-\\+\\.\\^:,]","");
+                } else if (replPhone2.startsWith("+")) {
+                    validPhoneNo = replPhone2.replaceAll("[\\-\\+\\.\\^:,]", "");
                     Log.e("TAG phone number +", validPhoneNo);
                 }
             } else {
@@ -154,38 +159,38 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
                     Toast.makeText(getApplicationContext(), "Airtel Number", Toast.LENGTH_LONG).show();
                     String replPhone3 = "null";
                     phoneCompany = "airtel";
-                    if(replPhone2.startsWith("0")){
+                    if (replPhone2.startsWith("0")) {
                         replPhone3 = replPhone2.replaceFirst("0", "\\254");
                         Log.e("TAG phone starts 0", replPhone3);
                         validPhoneNo = replPhone3;
-                    }else if(replPhone2.startsWith("7")){
+                    } else if (replPhone2.startsWith("7")) {
                         replPhone3 = replPhone2.replaceFirst("7", "\\254");
                         Log.e("TAG phone starts 7", replPhone3);
                         validPhoneNo = replPhone3;
-                    }else if(replPhone2.startsWith("+")){
-                        validPhoneNo = replPhone2.replaceAll("[\\-\\+\\.\\^:,]","");
+                    } else if (replPhone2.startsWith("+")) {
+                        validPhoneNo = replPhone2.replaceAll("[\\-\\+\\.\\^:,]", "");
                         Log.e("TAG phone number +", validPhoneNo);
                     }
-                }else {
+                } else {
                     patt = Pattern.compile(telkom);
                     match = patt.matcher(replPhone2);
                     if (match.find()) {
                         Toast.makeText(getApplicationContext(), "Telkom Number", Toast.LENGTH_LONG).show();
                         String replPhone3 = "null";
                         phoneCompany = "telkom";
-                        if(replPhone2.startsWith("0")){
+                        if (replPhone2.startsWith("0")) {
                             replPhone3 = replPhone2.replaceFirst("0", "\\254");
                             Log.e("TAG phone starts 0", replPhone3);
                             validPhoneNo = replPhone3;
-                        }else if(replPhone2.startsWith("7")){
+                        } else if (replPhone2.startsWith("7")) {
                             replPhone3 = replPhone2.replaceFirst("7", "\\254");
                             Log.e("TAG phone starts 7", replPhone3);
                             validPhoneNo = replPhone3;
-                        }else if(replPhone2.startsWith("+")){
-                            validPhoneNo = replPhone2.replaceAll("[\\-\\+\\.\\^:,]","");
+                        } else if (replPhone2.startsWith("+")) {
+                            validPhoneNo = replPhone2.replaceAll("[\\-\\+\\.\\^:,]", "");
                             Log.e("TAG phone number +", validPhoneNo);
                         }
-                    }else {
+                    } else {
                         Toast.makeText(getApplicationContext(), "Please enter a valid mobile number 'Safaricom only'", Toast.LENGTH_LONG).show();
                         Log.e("TAG phone No not check", replPhone2);
                         moveToContact(view);
@@ -201,9 +206,9 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
         return validPhoneNo;
     }
 
-    public void movetoPin(String phone,String amount) {
+    public void movetoPin(String phone, String amount) {
         Intent move = new Intent(this, EnterPin.class);
-        move.putExtra("Class","TopUpOtherNumber");
+        move.putExtra("Class", "TopUpOtherNumber");
         move.putExtra(EXTRA_PHONENAME, phoneName);
         move.putExtra(EXTRA_PROVIDER, phoneCompany);
         move.putExtra(EXTRA_PHONENUMBER, phone);
@@ -213,34 +218,39 @@ public class TopupOtherNumber extends AppCompatActivity  implements AdapterView.
 
     public void addAmount1(View view) {
 
-        TextView tvAdd =  findViewById(R.id.amount_display1);
+        TextView tvAdd = findViewById(R.id.amount_display1);
         String value = (String) tvAdd.getText();
 
-        EditText tvtext =  findViewById(R.id.amount_layout_other_number);
+        EditText tvtext = findViewById(R.id.amount_layout_other_number);
         tvtext.setText(value);
     }
 
     public void addAmount2(View view) {
-        TextView tvAdd =  findViewById(R.id.amount_display2);
+        TextView tvAdd = findViewById(R.id.amount_display2);
         String value = (String) tvAdd.getText();
 
-        EditText tvtext =  findViewById(R.id.amount_layout_other_number);
+        EditText tvtext = findViewById(R.id.amount_layout_other_number);
         tvtext.setText(value);
     }
 
     public void addAmount3(View view) {
-        TextView tvAdd =  findViewById(R.id.amount_display3);
+        TextView tvAdd = findViewById(R.id.amount_display3);
         String value = (String) tvAdd.getText();
 
-        EditText tvtext =  findViewById(R.id.amount_layout_other_number);
+        EditText tvtext = findViewById(R.id.amount_layout_other_number);
         tvtext.setText(value);
     }
 
     public void addAmount4(View view) {
-        TextView tvAdd =  findViewById(R.id.amount_display4);
+        TextView tvAdd = findViewById(R.id.amount_display4);
         String value = (String) tvAdd.getText();
 
-        EditText tvtext =  findViewById(R.id.amount_layout_other_number);
+        EditText tvtext = findViewById(R.id.amount_layout_other_number);
         tvtext.setText(value);
+    }
+
+    public void OpenQrCodeClass(View view) {
+        Intent intent = new Intent(getApplicationContext(), Qr_code22.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/wolanjej/pagerAdapters/Qr_codeAdapter.java
@@ -0,0 +1,43 @@
+package com.example.wolanjej.pagerAdapters;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.wolanjej.fragments.FragmentBookABus;
+import com.example.wolanjej.fragments.FragmentMyBusBookings;
+import com.example.wolanjej.fragments.fragment_qr_code;
+import com.example.wolanjej.fragments.fragment_scan_code;
+
+public class Qr_codeAdapter extends FragmentPagerAdapter {
+    private Context context;
+    int totalTabs;
+    public Qr_codeAdapter(Context context, @NonNull FragmentManager fm, int totalTabs) {
+        super(fm);
+        this.context  = context;
+        this.totalTabs = totalTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                fragment_qr_code fragment_qr_code = new fragment_qr_code();
+                return fragment_qr_code;
+            case 1:
+                fragment_scan_code fragment_scan_code = new fragment_scan_code();
+                return fragment_scan_code;
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
