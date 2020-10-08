1e53c222115b16b2a062c00e86aacf85e6f18dab
Guyoguyes <g.abduba43@gmail.com>
2020-10-08 22:54:54 +0300

    learning hub6 in-progress, choices remaining

---
+++ app/src/main/java/com/wolanjeAfrica/wolanjej/Learning_hub6.java
@@ -0,0 +1,14 @@
+package com.wolanjeAfrica.wolanjej;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Learning_hub6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_hub6);
+    }
+}
