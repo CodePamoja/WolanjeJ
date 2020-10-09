18426e664ff9b41e9b318feb7ce7cde099093c98
Guyoguyes <g.abduba43@gmail.com>
2020-10-09 15:49:51 +0300

    learning hub6 complete, bug onclick function

--- app/src/main/java/com/wolanjeAfrica/wolanjej/Learning_hub6.java
+++ app/src/main/java/com/wolanjeAfrica/wolanjej/Learning_hub6.java
@@ -3,12 +3,22 @@ package com.wolanjeAfrica.wolanjej;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class Learning_hub6 extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    MaterialRadioButton radioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_hub6);
+
+        radioGroup = findViewById(R.id.radioGroup);
+
     }
 }
