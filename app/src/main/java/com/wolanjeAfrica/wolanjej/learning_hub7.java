46dfda2c2ae90979cd2b617de202a2a212cb2a78
Guyoguyes <g.abduba43@gmail.com>
2020-10-10 06:51:01 +0300

    learning hub7 done

---
+++ app/src/main/java/com/wolanjeAfrica/wolanjej/learning_hub7.java
@@ -0,0 +1,14 @@
+package com.wolanjeAfrica.wolanjej;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class learning_hub7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_hub7);
+    }
+}
